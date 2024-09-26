cee512add362b8d0aba2b710595c9a230b2bbaff
Sarah Gille <sgille@ucsd.edu>
2024-09-25 18:41:12 -0700

    update to include use of SWOT simulator

--- swath_rossby_wave.py
+++ swath_rossby_wave.py
@@ -304,3 +304,208 @@ def build_hswath_matrix2(MSLA, MModes, k_n, l_n, lon,lat,lon_swath, lat_swath, i
     H_all[:, 1::2] = H_sin
     
     return H_all
+
+#-------------------------------------
+def read_simulator_error(start_day,ndays,index,index2):
+    import numpy as np
+    import scipy
+    import datetime as dt
+    from glob2 import glob
+    import xarray as xr
+
+    stats_output=np.zeros([ndays*2,6])
+    total_error=np.array([])
+    # start_date=dt.date(start_year,start_month,start_day)
+    # delta=dt.timedelta(days=1)
+    icount=0
+
+# read first ascending and descending files only for latitude, longitude, and cross track distance
+    # date_string=start_date.strftime("/home/sgille/swot_simulator/karin/%Y/SWOT_L2_LR_SSH_Expert_001_002_%Y%m%d*.nc")
+    # date_string=print('/home/sgille/swot_simulator/karin/%(Year)i/SWOT_L2_LR_SSH_Expert_%(counter)03d_002_*.nc' % {'Year': start_year, 'counter':icount+1}) 
+    simulator_files = sorted(glob('/home/sgille/swot_simulator/karin/20??/SWOT_L2_LR_SSH_Expert_' + str(start_day).zfill(3) + '_002*.nc'))
+    # simulator_files = sorted(glob(date_string))
+    print(simulator_files)
+    simulator_ds = xr.open_mfdataset(simulator_files, combine='nested', concat_dim = 'num_lines') # , engine='store', chunks={'time': 10})
+        
+    # date_string2=start_date.strftime("/home/sgille/swot_simulator/karin/%Y/SWOT_L2_LR_SSH_Expert_001_017_%Y%m%d*.nc")
+    # date_string2=print('/home/sgille/swot_simulator/karin/%(Year)i/SWOT_L2_LR_SSH_Expert_%(counter)03d_017_*.nc' % {'Year': start_year, 'counter':icount+1})
+    simulator_files2 = sorted(glob('/home/sgille/swot_simulator/karin/20??/SWOT_L2_LR_SSH_Expert_' + str(start_day).zfill(3) + '_017*.nc'))
+    #sorted(glob(date_string2))
+    simulator_ds2 = xr.open_mfdataset(simulator_files2, combine='nested', concat_dim = 'num_lines') 
+
+    latitude = simulator_ds['latitude'].values   
+    longitude = simulator_ds['longitude'].values-360
+    cross_track = simulator_ds['cross_track_distance'].values
+
+    latitude2 = simulator_ds2['latitude'].values   
+    longitude2 = simulator_ds2['longitude'].values-360
+    cross_track2 = simulator_ds2['cross_track_distance'].values
+
+    # sim_err_baseline_dilation = simulator_ds['simulated_error_baseline_dilation'].values
+    # sim_err_karin = simulator_ds['simulated_error_karin'].values
+    # sim_err_roll = simulator_ds['simulated_error_roll'].values
+    # sim_err_phase = simulator_ds['simulated_error_phase'].values
+    # sim_err_orbital = simulator_ds['simulated_error_orbital'].values
+    # sim_err_timing = simulator_ds['simulated_error_timing'].values
+    # # total_error1=sim_err_baseline_dilation + sim_err_karin + sim_err_roll + sim_err_phase + sim_err_orbital + sim_err_timing
+    # total_error1=sim_err_baseline_dilation + sim_err_karin + sim_err_roll + sim_err_phase + sim_err_timing
+    # stats_output[icount*2,0]=np.std(sim_err_baseline_dilation[index])
+    # stats_output[icount*2,1]=np.std(sim_err_karin[index])
+    # stats_output[icount*2,2]=np.std(sim_err_roll[index])
+    # stats_output[icount*2,3]=np.std(sim_err_phase[index])
+    # stats_output[icount*2,4]=np.std(sim_err_orbital[index])
+    # stats_output[icount*2,5]=np.std(sim_err_timing[index])
+
+    # sim_err_baseline_dilation2 = simulator_ds2['simulated_error_baseline_dilation'].values
+    # sim_err_karin2 = simulator_ds2['simulated_error_karin'].values
+    # sim_err_roll2 = simulator_ds2['simulated_error_roll'].values
+    # sim_err_phase2 = simulator_ds2['simulated_error_phase'].values
+    # sim_err_orbital2 = simulator_ds2['simulated_error_orbital'].values
+    # sim_err_timing2 = simulator_ds2['simulated_error_timing'].values
+    # # total_error2=sim_err_baseline_dilation2 + sim_err_karin2 + sim_err_roll2 + sim_err_phase2 + sim_err_orbital2 + sim_err_timing2
+    # total_error2=sim_err_baseline_dilation2 + sim_err_karin2 + sim_err_roll2 + sim_err_phase2 + sim_err_timing2
+    # stats_output[icount*2+1,0]=np.std(sim_err_baseline_dilation2[index])
+    # stats_output[icount*2+1,1]=np.std(sim_err_karin2[index])
+    # stats_output[icount*2+1,2]=np.std(sim_err_roll2[index])
+    # stats_output[icount*2+1,3]=np.std(sim_err_phase2[index])
+    # stats_output[icount*2+1,4]=np.std(sim_err_orbital2[index])
+    # stats_output[icount*2+1,5]=np.std(sim_err_timing2[index])
+    # total_error = np.concatenate((total_error1[index],total_error2[index2]))
+
+ 
+# now loop through all of the files to read error
+    for i in range(0,ndays):
+        simulator_files = sorted(glob('/home/sgille/swot_simulator/karin/20??/SWOT_L2_LR_SSH_Expert_' + str(start_day+i).zfill(3) + '_002*.nc'))
+        simulator_ds = xr.open_mfdataset(simulator_files, combine='nested', concat_dim = 'num_lines') # , engine='store', chunks={'time': 10})
+        
+        simulator_files2 = sorted(glob('/home/sgille/swot_simulator/karin/20??/SWOT_L2_LR_SSH_Expert_' + str(start_day+i).zfill(3) + '_017*.nc'))
+        simulator_ds2 = xr.open_mfdataset(simulator_files2, combine='nested', concat_dim = 'num_lines') 
+
+        sim_err_baseline_dilation = simulator_ds['simulated_error_baseline_dilation'].values
+        sim_err_karin = simulator_ds['simulated_error_karin'].values
+        sim_err_roll = simulator_ds['simulated_error_roll'].values
+        sim_err_phase = simulator_ds['simulated_error_phase'].values
+        sim_err_orbital = simulator_ds['simulated_error_orbital'].values
+        sim_err_timing = simulator_ds['simulated_error_timing'].values
+        # total_error1=sim_err_baseline_dilation + sim_err_karin + sim_err_roll + sim_err_phase + sim_err_orbital + sim_err_timing
+        total_error1=sim_err_baseline_dilation + sim_err_karin + sim_err_roll + sim_err_phase + sim_err_timing
+        stats_output[i*2,0]=np.std(sim_err_baseline_dilation[index])
+        stats_output[i*2,1]=np.std(sim_err_karin[index])
+        stats_output[i*2,2]=np.std(sim_err_roll[index])
+        stats_output[i*2,3]=np.std(sim_err_phase[index])
+        stats_output[i*2,4]=np.std(sim_err_orbital[index])
+        stats_output[i*2,5]=np.std(sim_err_timing[index])           
+        
+        sim_err_baseline_dilation2 = simulator_ds2['simulated_error_baseline_dilation'].values
+        sim_err_karin2 = simulator_ds2['simulated_error_karin'].values
+        sim_err_roll2 = simulator_ds2['simulated_error_roll'].values
+        sim_err_phase2 = simulator_ds2['simulated_error_phase'].values
+        sim_err_orbital2 = simulator_ds2['simulated_error_orbital'].values
+        sim_err_timing2 = simulator_ds2['simulated_error_timing'].values
+        # total_error2=sim_err_baseline_dilation2 + sim_err_karin2 + sim_err_roll2 + sim_err_phase2 + sim_err_orbital2 + sim_err_timing2
+        total_error2=sim_err_baseline_dilation2 + sim_err_karin2 + sim_err_roll2 + sim_err_phase2 + sim_err_timing2
+        stats_output[i*2+1,0]=np.std(sim_err_baseline_dilation2[index2])
+        stats_output[i*2+1,1]=np.std(sim_err_karin2[index2])
+        stats_output[i*2+1,2]=np.std(sim_err_roll2[index2])
+        stats_output[i*2+1,3]=np.std(sim_err_phase2[index2])
+        stats_output[i*2+1,4]=np.std(sim_err_orbital2[index2])
+        stats_output[i*2+1,5]=np.std(sim_err_timing2[index2])
+        total_error = np.concatenate((total_error,total_error1[index],total_error2[index2]))
+
+    return total_error, stats_output
+
+
+#     day_use = np.zeros(MSLA.shape[2])
+    
+    count_max = len(lon_swath)
+    # count = 0
+    # for tt in range(MSLA.shape[2]):
+    #     for jj in range(MSLA.shape[0]):
+    #         for ii in range(MSLA.shape[1]):
+    #             if (MSLA[jj:jj+1,ii,tt].mask==False):
+    #                 SSHA_vector[count] = MSLA[jj, ii, tt]
+    #                 day_use[count]=day+tt
+    #                 Iindex[count], Jindex[count], Tindex[count] = int(ii), int(jj), int(tt)
+    #                 count = count + 1
+
+    # count_max=count
+    ndays=MSLA.shape[2]
+    H_cos, H_sin = np.zeros([ndays*count_max,M]), np.zeros([ndays*count_max, M])
+    H_all = np.zeros([ndays*count_max, M * 2])
+    
+    # print(count_max, MModes, k_n, l_n)
+    nn = 0     
+    for kk in range(len(k_n)):
+        for ll in range(len(l_n)):
+            for mm in range(MModes):
+                omega[kk, ll, mm] = Beta * k_n[kk, mm] / (k_n[kk, mm] ** 2 + l_n[ll, mm] ** 2 + Rm[mm] ** -2)          
+                idata=0
+                for tt in range(MSLA.shape[2]):
+                    for count in range(count_max):
+                        H_cos[idata, nn] = Psi[0, mm] * np.cos(k_n[kk, mm] * dlon_swath[count]+ l_n[ll, mm] * dlat_swath[count] + omega[kk, ll, mm] * T_time[int(day+tt)])
+                        H_sin[idata, nn] = Psi[0, mm] * np.sin(k_n[kk, mm] * dlon_swath[count]+ l_n[ll, mm] * dlat_swath[count] + omega[kk, ll, mm] * T_time[int(day+tt)])
+                        idata += 1
+                nn += 1 
+
+#--------------------------------------------------------------
+def build_hswath_matrix2_for_simulator(MSLA, MModes, k_n, l_n, lon,lat,lon_swath, lat_swath, index, T_time, Psi, Rm, day):
+    
+    '''
+    Build H matrix or basis function for Rossby wave model.
+    
+    Input:
+    SSHA_vector: SSH anomalies as a vector,
+    Psi (horizontal velocity and pressure structure functions), 
+    k_n (zonal wavenumber), 
+    l_n (latitudional wavenumber), 
+    frequency, 
+    longitude, latitude and time. 
+    
+    Output: H matrix for Rossby wave model
+    
+    '''
+    
+    import numpy as np
+    
+    Phi0 = lat.mean() # central latitude (φ0)
+    Omega = 7.27e-5 # Ω is the angular speed of the earth
+    Earth_radius = 6.371e6 / 1e5 # meters
+    Beta = 2 * Omega * np.cos(Phi0*np.pi/180.) / Earth_radius 
+    f0 = 2 * Omega * np.sin(Phi0*np.pi/180.) 
+
+    dlon = lon - lon.mean()
+    dlat = lat - lat.mean()
+    dlon_swath = lon_swath - lon.mean()
+    dlat_swath = lat_swath -lat.mean()
+    #print('lon',lon.mean(),'lat',lat.mean())
+    M = len(k_n) * len(l_n)
+
+    omega = np.zeros([len(k_n), len(l_n), MModes])
+#     day_use = np.zeros(MSLA.shape[2])
+    
+    count_max = len(lon_swath)
+
+    ndays=MSLA.shape[2]
+    H_cos, H_sin = np.zeros([ndays*count_max,M]), np.zeros([ndays*count_max, M])
+    H_all = np.zeros([ndays*count_max, M * 2])
+    
+    # print(count_max, MModes, k_n, l_n)
+    nn = 0     
+    for kk in range(len(k_n)):
+        for ll in range(len(l_n)):
+            for mm in range(MModes):
+                omega[kk, ll, mm] = Beta * k_n[kk, mm] / (k_n[kk, mm] ** 2 + l_n[ll, mm] ** 2 + Rm[mm] ** -2)          
+                idata=0
+                for tt in range(MSLA.shape[2]):
+                    for count in range(count_max):
+                        H_cos[idata, nn] = Psi[0, mm] * np.cos(k_n[kk, mm] * dlon_swath[count]+ l_n[ll, mm] * dlat_swath[count] + omega[kk, ll, mm] * T_time[int(day+tt)])
+                        H_sin[idata, nn] = Psi[0, mm] * np.sin(k_n[kk, mm] * dlon_swath[count]+ l_n[ll, mm] * dlat_swath[count] + omega[kk, ll, mm] * T_time[int(day+tt)])
+                        idata += 1
+                nn += 1 
+    
+    omega = np.zeros([len(k_n), len(l_n), MModes])  
+    
+    H_all[:, 0::2] = H_cos 
+    H_all[:, 1::2] = H_sin
+    
+    return H_all
